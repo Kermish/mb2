b93725e50814882895ba63a207697b941d1ba089
Kermish <41496169+Kermish@users.noreply.github.com>
2019-08-29 21:43:13 +1000

    manual upload - correct error handling?

--- trylocalstorage.js
+++ trylocalstorage.js
@@ -1,6 +1,6 @@
 var mymap , absAccelNow;
 
-
+/*
 function storageAvailable(type) {
     var storage;
     try {
@@ -36,7 +36,7 @@ else {
 // Too bad, no localStorage for us
 alert( "boo" );
 }
-
+*/
 
 
 
@@ -50,13 +50,13 @@ function setUserOked(){UserOked=true};
 
 
 bilicon = {
-path: 'M16 58 L32 59 L48 58 L48 12 L44 8 L20 8 L16 12 L16 30 L17 30 L20 23 L32 19 L44 23 L47 30 L32 26 L25.166666666666664 27.5 L16 30 L16 58 L16 58 L16 58 Z',fillColor: 'black',
-fillOpacity: 0.5,
-strokeColor: '#fff',
-strokeWeight: 1,
-scale: 0.5,
-anchor: new google.maps.Point(32, 32),
-rotation: 0,
+    path: 'M16 58 L32 59 L48 58 L48 12 L44 8 L20 8 L16 12 L16 30 L17 30 L20 23 L32 19 L44 23 L47 30 L32 26 L25.166666666666664 27.5 L16 30 L16 58 L16 58 L16 58 Z',fillColor: 'black',
+    fillOpacity: 0.5,
+    strokeColor: '#fff',
+    strokeWeight: 1,
+    scale: 0.5,
+    anchor: new google.maps.Point(32, 32),
+    rotation: 0,
 }
 
 
@@ -81,33 +81,51 @@ function scrollMap(position) {
     mymap.setCenter( new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
     console.log(`bilMarker ${bilMarker}`);
     bilMarker.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-    
-    }
-    function handleError(error) {
-    // Update a div element with error.message.
+}
+
+function handleError(error) {
+    console.log(error);
 }
 //>>>>>>>>>>>>>>>>GeoLocation section end <<<<<<<<<<<<<<<<<<<<<<<<<
 // Request repeated updates.
 var myLatLng = {
     lat: -33.782,
     lng: 151.244
-  };
+};
 
-  //>>>>>>>>>>>>>>>>Accelerometer Section<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-  let sensor = new Accelerometer();
-  sensor.start();
-  sensor.onreading = () => {
-    console.log("Acceleration along X-axis: " + sensor.x);
-    console.log("Acceleration along Y-axis: " + sensor.y);
-    console.log("Acceleration along Z-axis: " + sensor.z);
-    absAccelNow=Math.hypot(sensor.x,sensor.y,sensor.z);
-    console.log(`absAccellNow = ${absAccelNow}`);
-  }
-  sensor.onerror = event => console.log(event.error.name, event.error.message);
-  //>>>>>>>>>>>>>>>>Accelerometer Section Ends<<<<<<<<<<<<<<<<<<<<<<<
+//>>>>>>>>>>>>>>>>Accelerometer Section<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+let sensor = null;
+try { 
+    sensor = new Accelerometer({frequency:2});
+    sensor.addEventListener('error', event => {
+        // Handle runtime errors.
+        if (event.error.name === 'NotAllowedError') {
+            console.log('Permission to access sensor was denied.');
+        } else if (event.error.name === 'NotReadableError' ) {
+            console.log('Cannot connect to the sensor.');
+        }
+    });
+    accelerometer.addEventListener('reading', () => {
+        console.log("Acceleration along X-axis: " + sensor.x);
+        console.log("Acceleration along Y-axis: " + sensor.y);
+        console.log("Acceleration along Z-axis: " + sensor.z);
+        absAccelNow=Math.hypot(sensor.x,sensor.y,sensor.z);
+        console.log(`absAccellNow = ${absAccelNow}`);
+    });
+    sensor.start();
+} catch (error){
+    // Handle construction errors.
+    if (error.name === 'SecurityError') {
+        console.log('Sensor construction was blocked by the Feature Policy.');
+    } else if (error.name === 'ReferenceError') {
+        console.log('Sensor is not supported by the User Agent.');
+    } else {
+        throw error;
+    }
+}
 
-
-  mymap = new google.maps.Map(document.getElementById('map_canvas'), {
+//>>>>>>>>>>>>>>>>Accelerometer Section Ends<<<<<<<<<<<<<<<<<<<<<<<
+mymap = new google.maps.Map(document.getElementById('map_canvas'), {
     center: myLatLng,
     zoom: 18,
     mapTypeId: 'satellite',
@@ -117,13 +135,13 @@ var myLatLng = {
     streetViewControl: false,
     rotateControl: false,
     fullscreenControl: false
-  });
-  bilMarker = new google.maps.Marker({
+});
+bilMarker = new google.maps.Marker({
     position: mymap.getCenter(),
     icon: bilicon,
     draggable: false,
     map: mymap
-  });
+});
 
 var watchId = navigator.geolocation.watchPosition(scrollMap, handleError);
 
